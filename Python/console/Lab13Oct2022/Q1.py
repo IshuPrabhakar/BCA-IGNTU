65496a47094087d0618019051fd2ab434d832ad3
IshuPrabhakar <ishuprabhakar3679@outlook.com>
2022-10-18 12:51:52 +0530

    18 oct

---
+++ Python/console/Lab13Oct2022/Q1.py
@@ -0,0 +1,9 @@
+# Program to Demonstrate List in python
+
+# creating list
+my_list = [10,20,30,40]
+
+print(my_list)
+
+my_list.remove(30)
+print(my_list)
---
+++ Python/console/Lab13Oct2022/Q2.py
@@ -0,0 +1,11 @@
+# 18 Python program to demonstrate the use Tuple
+
+my_tuple = (10,20,30)
+
+print(my_tuple)
+
+my_tuple = ("hello", "sir")
+
+print(my_tuple)
+
+print(my_tuple is my_tuple)
